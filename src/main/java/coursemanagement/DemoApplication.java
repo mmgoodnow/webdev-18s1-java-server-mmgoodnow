2c5cffa9579ca1552e406a20e53658299e544d81
Michael Goodnow <mmgoodnow@gmail.com>
2018-05-10 14:22:38 -0400

    set up dir structure

--- src/main/java/coursemanagement/DemoApplication.java
+++ src/main/java/coursemanagement/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.mgoodnowcoursemanagement;
+package coursemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
