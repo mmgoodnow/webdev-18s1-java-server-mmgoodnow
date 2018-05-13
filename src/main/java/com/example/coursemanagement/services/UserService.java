bb4c178e2d32ba8206d4b0ab9fd6967c69a82b3f
Michael Goodnow <mmgoodnow@gmail.com>
2018-05-12 21:54:36 -0400

    add delete and create functionality to client user admin page, begin profile page

--- src/main/java/com/example/coursemanagement/services/UserService.java
+++ src/main/java/com/example/coursemanagement/services/UserService.java
@@ -6,6 +6,7 @@ import com.example.coursemanagement.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +33,8 @@ public class UserService {
 		return (List<User>) repo.findAll();
 	}
 
-	@DeleteMapping("/api/user")
-	public void deleteUser(@RequestBody User user) {
-
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		repo.deleteById(id);
 	}
 }
