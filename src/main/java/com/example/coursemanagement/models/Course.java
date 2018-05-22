8a3c584188fe67a343b32f355ef67d33b77c24f4
Michael Goodnow <mmgoodnow@gmail.com>
2018-05-21 22:33:57 -0400

    import list

--- src/main/java/com/example/coursemanagement/models/Course.java
+++ src/main/java/com/example/coursemanagement/models/Course.java
@@ -1,6 +1,7 @@
 package com.example.coursemanagement.models;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
--- src/main/java/com/example/coursemanagement/models/Module.java
+++ src/main/java/com/example/coursemanagement/models/Module.java
@@ -2,6 +2,8 @@ package com.example.coursemanagement.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
