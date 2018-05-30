78e4447fe93cf1561916ffdb922d2c8a7fc83651
Michael Goodnow <mmgoodnow@gmail.com>
2018-05-30 18:05:32 -0400

    add widget base classˆ

---
+++ src/main/java/webdev/models/Widget.java
@@ -0,0 +1,15 @@
+package webdev.models;
+
+/**
+ * Created by Michael Goodnow on 5/30/18.
+ */
+
+public abstract class Widget {
+	String name;
+	int order;
+	String text;
+	String className;
+	String style;
+	String width;
+	String height;
+}
