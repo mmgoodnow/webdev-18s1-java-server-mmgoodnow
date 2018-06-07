19f2e096de2d09363a823354ff4d33f414a7e2a3
Michael Goodnow <mmgoodnow@gmail.com>
2018-06-07 00:03:32 -0400

    implement essay question service skeleton BEˆ

--- src/main/java/webdev/services/questions/EssayQuestionService.java
+++ src/main/java/webdev/services/questions/EssayQuestionService.java
@@ -1,7 +1,8 @@
 package webdev.services.questions;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import webdev.models.questions.EssayExamQuestion;
 
 /**
  * Created by Michael Goodnow on 6/6/18.
@@ -10,4 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class EssayQuestionService {
+
+	@PostMapping("/api/exam/{eid}/essay")
+	public EssayExamQuestion createEssayQuestion(@PathVariable("eid") int eid,
+	                                             @RequestBody EssayExamQuestion e) {
+
+	}
+
+	@PutMapping("/api/essay/{id}")
+	public EssayExamQuestion updateEssayQuestion(@PathVariable("id") int id) {
+
+	}
+
+	@GetMapping("/api/essay/{id}")
+	public EssayExamQuestion getEssayQuestion(@PathVariable("id") int id) {
+
+	}
+
+	@DeleteMapping("/api/essay/{id}")
+	public void deleteEssayQuestion(@PathVariable("id") int id) {
+
+	}
 }
