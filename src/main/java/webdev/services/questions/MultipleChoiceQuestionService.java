06ab342871bec28298350e50e0b9be7f4c2bef43
Michael Goodnow <mmgoodnow@gmail.com>
2018-06-07 00:50:56 -0400

    implement multiple choice and true false question service BEˆ

--- src/main/java/webdev/services/questions/MultipleChoiceQuestionService.java
+++ src/main/java/webdev/services/questions/MultipleChoiceQuestionService.java
@@ -1,7 +1,15 @@
 package webdev.services.questions;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import webdev.models.questions.MultipleChoiceExamQuestion;
+import webdev.models.widgets.Exam;
+import webdev.repositories.questions.MultipleChoiceQuestionRepository;
+import webdev.repositories.widgets.ExamRepository;
 
 /**
  * Created by Michael Goodnow on 6/6/18.
@@ -10,6 +18,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class MultipleChoiceQuestionService {
+	@Autowired
+	ExamRepository examRepo;
 
+	@Autowired
+	MultipleChoiceQuestionRepository repo;
+
+	@PostMapping("/api/exam/{eid}/choice")
+	public MultipleChoiceExamQuestion createChoiceQuestion(@PathVariable("eid") int eid,
+	                                                       @RequestBody MultipleChoiceExamQuestion
+		                                                       question) {
+		Optional<Exam> opt = examRepo.findById(eid);
+		if (opt.isPresent()) {
+			Exam exam = opt.get();
+			question.setExam(exam);
+			return repo.save(question);
+		}
+		return null;
+	}
+
+	@PutMapping("/api/choice/{id}")
+	public MultipleChoiceExamQuestion updateChoiceQuestion(@PathVariable("id") int id,
+	                                                       @RequestBody MultipleChoiceExamQuestion
+		                                                       newQuestion) {
+		Optional<MultipleChoiceExamQuestion> opt = repo.findById(id);
+		if (opt.isPresent()) {
+			MultipleChoiceExamQuestion oldQuestion = opt.get();
+			newQuestion.setId(id);
+			return repo.save(newQuestion);
+		}
+		throw new NoSuchElementException();
+	}
+
+	@GetMapping("/api/choice/{id}")
+	public MultipleChoiceExamQuestion getChoiceQuestion(@PathVariable("id") int id) {
+		return repo.findById(id).orElse(null);
+	}
+
+	@DeleteMapping("/api/choice/{id}")
+	public void deleteChoiceQuestion(@PathVariable("id") int id) {
+		repo.deleteById(id);
+	}
 
 }
--- src/main/java/webdev/services/questions/TrueOrFalseQuestionService.java
+++ src/main/java/webdev/services/questions/TrueOrFalseQuestionService.java
@@ -1,7 +1,15 @@
 package webdev.services.questions;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import webdev.models.questions.TrueOrFalseExamQuestion;
+import webdev.models.widgets.Exam;
+import webdev.repositories.questions.TrueOrFalseQuestionRepository;
+import webdev.repositories.widgets.ExamRepository;
 
 /**
  * Created by Michael Goodnow on 6/6/18.
@@ -10,4 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class TrueOrFalseQuestionService {
+	@Autowired
+	ExamRepository examRepo;
+
+	@Autowired
+	TrueOrFalseQuestionRepository repo;
+
+	@PostMapping("/api/exam/{eid}/truefalse")
+	public TrueOrFalseExamQuestion createTrueFalseQuestion(@PathVariable("eid") int eid,
+	                                                       @RequestBody TrueOrFalseExamQuestion
+		                                                       question) {
+		Optional<Exam> opt = examRepo.findById(eid);
+		if (opt.isPresent()) {
+			Exam exam = opt.get();
+			question.setExam(exam);
+			return repo.save(question);
+		}
+		return null;
+	}
+
+	@PutMapping("/api/truefalse/{id}")
+	public TrueOrFalseExamQuestion updateTrueFalseQuestion(@PathVariable("id") int id,
+	                                                       @RequestBody TrueOrFalseExamQuestion
+		                                                       newQuestion) {
+		Optional<TrueOrFalseExamQuestion> opt = repo.findById(id);
+		if (opt.isPresent()) {
+			TrueOrFalseExamQuestion oldQuestion = opt.get();
+			newQuestion.setId(id);
+			return repo.save(newQuestion);
+		}
+		throw new NoSuchElementException();
+	}
+
+	@GetMapping("/api/truefalse/{id}")
+	public TrueOrFalseExamQuestion getTrueFalseQuestion(@PathVariable("id") int id) {
+		return repo.findById(id).orElse(null);
+	}
+
+	@DeleteMapping("/api/truefalse/{id}")
+	public void deleteTrueFalseQuestion(@PathVariable("id") int id) {
+		repo.deleteById(id);
+	}
 }
